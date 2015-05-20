1781c34e4a93013090300ea39b8b0c446ba63901
crussi <chrisrussi@gmail.com>
2015-05-19 22:16:16 -0700

    Beginning work on unauthenticated layout

--- client/js/FlowRouter.js
+++ client/js/FlowRouter.js
@@ -2,14 +2,31 @@
 
 FlowRouter.route('/',{
     subscriptions: function(params) {
-        console.log('flow sub');
+        console.log('route / ... flow sub');
         this.register('menuItems', Meteor.subscribe('menu-items'));
+    },
+    action: function() {
+        console.log('route / ... flow action');
+        //FlowLayout.render('menu');
+        //if (!Meteor.user()) {
+            FlowLayout.render('layout-unauth', { header: "header-unauth", content: "content-mktg"});
+        //    FlowLayout.render('layout', { header: "header", content: "content"});
+        //}
+
+    }
+});
+
+FlowRouter.route('/Signin',{
+    subscriptions: function(params) {
+        console.log('flow sub');
+
     },
     action: function() {
         console.log('flow action');
         //FlowLayout.render('menu');
         //if (!Meteor.user()) {
-            FlowLayout.render('layout-unauth', { header: "header-unauth", content: "content-mktg"});
+        FlowLayout.render('layout-unauth', { header: "header-unauth", content: "content-mktg"});
+        //    FlowLayout.render('layout', { header: "header", content: "content"});
         //}
 
     }
