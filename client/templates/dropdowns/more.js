71c2b45f9b3f5f927d8b395a9fec1078fa7c5e7e
crussi <chrisrussi@gmail.com>
2015-07-04 10:57:10 -0700

    fixed sub-menu closing issue

--- client/templates/dropdowns/more.js
+++ client/templates/dropdowns/more.js
@@ -1,17 +1,14 @@
 
 Template['moredropdown'].events({
-    "click li": function (event, template) {
-        event.preventDefault();
-        handleDdMenuClick(event.currentTarget.id);
+    "click li": function (e) {
+        e.preventDefault();
+        handleDdMenuClick(e.currentTarget.id);
     }
 });
 
 handleDdMenuClick = function(id) {
     var a = $('#' + id + ' a:first-of-type');
     var action = $(a).data("action");
-    //var fullscreenElement = document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement;
-    var fullscreenEnabled = document.fullscreenEnabled || document.mozFullScreenEnabled || document.webkitFullscreenEnabled;
-    //id = $(target).attr('id');
     switch(action) {
         case 'fullscreen':
             var isFullScreen = document.fullScreen ||
@@ -26,9 +23,7 @@ handleDdMenuClick = function(id) {
             break;
         case 'clear-localstorage':
             console.log(action);
-            //e.preventDefault();
-
-            swal({
+             swal({
                 title: "Are you sure?",
                 text: "Local storage, for this application, will be deleted",
                 type: "warning",
